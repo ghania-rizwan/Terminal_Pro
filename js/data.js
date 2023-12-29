b8dd45a6d753ee7c7e1dca7eae0a116de3ff4c26
Ghania <ghania1422@gmail.com>
2023-12-29 14:01:20 +0500

    final

---
+++ js/data.js
@@ -0,0 +1,26 @@
+var data = {
+  name : [
+    {name: 'Name', value: 'Ghania Rizwan'}
+  ],
+  bio: [
+    {name: 'Name', value: 'Ghania Rizwan'},
+    {name: 'Email', value: 'ghania1422@gmail.com'},
+    {name: 'Phone', value: '+92-332-3817101'},
+    {name: 'Country', value: 'Pakistan'},
+    {name: 'City', value: 'Karachi'}
+  ],
+  github: [
+    {name: 'GitHub', value: 'ghania-rizwan'}
+  ],
+  projects: [
+    {name: 'Article Summarizer', value: 'https://github.com/ghania-rizwan/Ezzy-ArticleSummarizer'},
+    {name: 'Weather Forecast', value: 'https://github.com/ghania-rizwan/weather_forecaste'},
+    {name: 'Flappy Bird Game', value: 'hhttps://github.com/ghania-rizwan/FlappyBirdGame'},
+    {name: 'Pinterest', value: 'https://github.com/ghania-rizwan/pinterest'},
+  ],
+  resume: 'Ghania_Rizwan_CV.pdf',
+  socials: [
+    {name: 'LinkedIn', value: 'https://www.linkedin.com/in/ghania-rizwan-4ba77a1b0/'},
+    {name: 'GitHub', value: 'https://github.com/ghania-rizwan'}
+  ]
+}
